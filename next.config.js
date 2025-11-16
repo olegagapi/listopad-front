21032db49db479a7e19ab7d37a3b196a6ec3fa1a
Oleg Agapov <oleg.agapov@gmail.com>
2025-11-16 15:52:11 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dl.airtableusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "v5.airtableusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "dl.airtable.com",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
