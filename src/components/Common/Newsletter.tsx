8e464f36fed5f3fab197393265b80fe5dc6b8858
Oleg Agapov <oleg.agapov@gmail.com>
2026-01-28 02:00:26 +0200

    fixed dont miss out banner

--- src/components/Common/Newsletter.tsx
+++ src/components/Common/Newsletter.tsx
@@ -1,32 +1,19 @@
 import React from "react";
-import Image from "next/image";
 import { useTranslations } from "next-intl";
 
 const Newsletter = () => {
   const t = useTranslations("Newsletter");
 
   return (
-    <section className="overflow-hidden">
+    <section className="overflow-hidden mt-12 lg:mt-16">
       <div className="max-w-[1170px] mx-auto px-4 sm:px-8 xl:px-0">
-        <div className="relative z-1 overflow-hidden rounded-xl">
-          {/* <!-- bg shapes --> */}
-          <Image
-            src="/images/shapes/newsletter-bg.jpg"
-            alt="background illustration"
-            className="absolute -z-1 w-full h-full left-0 top-0 rounded-xl"
-            width={1170}
-            height={200}
-          />
-          <div className="absolute -z-1 max-w-[523px] max-h-[243px] w-full h-full right-0 top-0 bg-gradient-1"></div>
-
+        <div className="relative z-1 overflow-hidden rounded-xl bg-gradient-to-br from-darkslate-light to-darkslate-dark">
           <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-8 px-4 sm:px-7.5 xl:pl-12.5 xl:pr-14 py-11">
             <div className="max-w-[491px] w-full">
               <h2 className="max-w-[399px] text-white font-bold text-lg sm:text-xl xl:text-heading-4 mb-3">
                 {t("title")}
               </h2>
-              <p className="text-white">
-                {t("desc")}
-              </p>
+              <p className="text-white">{t("desc")}</p>
             </div>
 
             <div className="max-w-[477px] w-full">
